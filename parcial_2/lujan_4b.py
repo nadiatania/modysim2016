6e8a7e03b593a694f70ce1eeb5c6ce6f841cabba
ntlujan <nadia.lujan@intel.com>
2016-05-08 20:36:09 -0300

    Compliance with pep8

--- parcial_2/lujan_4b.py
+++ parcial_2/lujan_4b.py
@@ -1,20 +1,23 @@
 from random import random
 import math
 
-def g(s,t):
-	e=math.e**(-((1.0/t-1.0)+(s+1.0))**2)
-	return e*(1.0/t**2)*(1.0/t-1.0)
+
+def g(s, t):
+    e = math.e**(-((1.0/t-1.0)+(s+1.0))**2)
+    return e*(1.0/t**2)*(1.0/t-1.0)
+
 
 def experimento(n):
-	result = 0.0
-	for i in range(n):
-		u=random() #genero la variable aleatoria u
-		v=random() #genero la variable aletroia v
-		result += g(u,v)
-	return float(result/n)
+    result = 0.0
+    for i in range(n):
+        # genero la variable aleatoria u
+        u = random()
+        # genero la variable aletroia v
+        v = random()
+        result += g(u, v)
+    return float(result/n)
 
 print experimento(100)
 print experimento(1000)
 print experimento(10000)
 print experimento(100000)
-	
--- parcial_2/lujan_5b.py
+++ parcial_2/lujan_5b.py
@@ -1,19 +1,20 @@
 from random import random
 
-def experimento(n):
-	exitos=0.0
-	for i in range(n):
-		u=random()
-		v=random()
-		#si el primero es menos a 1/2 y el segundo es mayor que el primero
-		if(u<0.5 and v>u):
-			exitos += 1.0
-		#si el primero es mayor a 1/2 y el segundo es menos al primero
-		elif(u>0.5 and v<u):
-			exitos += 1.0
 
-	return float(exitos/n)
-		
+def experimento(n):
+    exitos = 0.0
+    for i in range(n):
+        u = random()
+        v = random()
+        # si el primero es menos a 1/2 y el segundo es mayor que el primero
+        if(u < 0.5 and v > u):
+            exitos += 1.0
+        # si el primero es mayor a 1/2 y el segundo es menos al primero
+        elif(u > 0.5 and v < u):
+            exitos += 1.0
+
+    return float(exitos/n)
+
 print experimento(100)
 print experimento(1000)
 print experimento(10000)
